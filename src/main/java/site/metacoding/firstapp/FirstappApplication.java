fb4b2ad445c3129df28dca72a61337c51ff17014
seong0210 <dlgustjd9566@gmail.com>
2022-09-28 17:33:31 +0900

    테이블 생성 완료

--- src/main/java/site/metacoding/firstapp/FirstappApplication.java
+++ src/main/java/site/metacoding/firstapp/FirstappApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstappApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FirstappApplication.class, args);
+
 	}
 
 }
