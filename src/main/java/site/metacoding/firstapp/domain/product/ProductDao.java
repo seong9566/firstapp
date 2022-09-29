b1e2a06f91f6f29d7d4597b43828c874e6c5e0d1
seong0210 <dlgustjd9566@gmail.com>
2022-09-29 17:53:11 +0900

    findById테스트완료, 추후 수정 필요함

--- src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
+++ src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
@@ -1,5 +1,17 @@
 package site.metacoding.firstapp.domain.product;
 
-public interface ProductDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+public interface ProductDao {
+    public Product findById(Integer productId);
+
+    public List<Product> findAll();
+
+    public void insert(Product product);
+
+    public void update(Product product);
+
+    public void deleteById(Integer productId);
 }
--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -1,5 +1,34 @@
 package site.metacoding.firstapp.domain.product;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+//SpringBootTest 모든것을 다 메모리에 띄움
+//Vscode에서 import 자동 입력 키 : alt + shift + o 
+
+@SpringBootTest
 public class ProductDaoTest {
 
+    // 단위 테스트는 생성자 주입이 안된다.
+    // 외부에서 주입할때 빈 생성자만 주입해준다. -> 생성자 주입이 되지 않는다.
+    @Autowired
+    private ProductDao productDao;
+
+    // test는 절대 return이 안된다 void사용
+    @Test
+    public void findById_test() {
+        // given - 받아야 할 것 ex) delete에선 id가 필요하다 id를 임의로 준다
+        Integer productId = 1;
+
+        // when - 테스트
+        Product productPS = productDao.findById(productId);
+
+        // then - 검증
+        assertEquals("바나나", productPS.getProductName());
+    }
 }
